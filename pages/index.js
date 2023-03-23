8662ca908ca24f5857c6a9c7db792df7a2616335
Tim Kast <timkast93@gmx.de>
2023-03-23 11:17:59 +0100

    edit button and minor changes

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import styled from "styled-components";
-import Link from "next/link";
 import Navigation from "@/components/Navigation";
 
 export default function Home() {
@@ -7,10 +6,16 @@ export default function Home() {
     <div>
       <Heading>Shelter Manager</Heading>
     </div>
-  
   );
 }
 
 const Heading = styled.h1`
-text-align: center;
+  display: flex;
+  justify-content: center;
+  width: 270px;
+  background-color: blue;
+  color: white;
+  border: 2px solid;
+  border-radius: 15px;
+  border-color: black;
 `;
